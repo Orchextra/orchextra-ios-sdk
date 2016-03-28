cb56f641a4ed3f803c3a0f69559ee276007e7c80
Judith Medina <judith.medina@gigigo.com>
2016-03-28 15:50:19 +0200

    - Travis: Added script to run travis.

---
+++ script.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+TEST_CMD="xcodebuild -scheme Orchextra -project Orchextra.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.3' build test"
+
+which -s xcpretty
+XCPRETTY_INSTALLED=$?
+
+if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
+  eval "${TEST_CMD} | xcpretty"
+else
+  eval "$TEST_CMD"
+fi
