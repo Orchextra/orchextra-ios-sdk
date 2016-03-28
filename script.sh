482703788ee39f8b518e9a51cd7760b92b60e4a2
Judith Medina <judith.medina@gigigo.com>
2016-03-28 17:08:30 +0200

    - Travis: Update script.sh file, included device simulator.

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 
-TEST_CMD="xcodebuild -scheme Orchextra -project Orchextra.xcodeproj -sdk iphonesimulator build test"
+TEST_CMD="xcodebuild -scheme Orchextra -project Orchextra.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.2' build test"
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
