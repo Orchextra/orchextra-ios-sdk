60d7c03540636b2f3c1f5a148c2e44cd59b7f6fb
Carlos Vicente <carlos.vicente@gigigo.com>
2017-08-02 14:49:07 +0200

    - Updated Orchextra SDK version constant and README

--- Orchextra/SDK/ORCConstants.m
+++ Orchextra/SDK/ORCConstants.m
@@ -33,7 +33,7 @@ NSString * const ORCSchemeImageRecognition = @"Orchextra://imageRecognition";
 
 #pragma mark - SDK
     
-NSString * const ORCSDKVersion = @"2.1.5";
+NSString * const ORCSDKVersion = @"2.1.6";
 
 #pragma mark - Network
 
