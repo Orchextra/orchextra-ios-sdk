f256b23da6d7585f431b6d9a7133776e8cb755c8
Judith Medina <judith.medina@gigigo.com>
2017-10-10 11:40:09 +0200

    Fixing bugs testing storageproximity

--- OrchextraTests/Mocks/Modules/ProximityMocks/StorageProximityMock.swift
+++ OrchextraTests/Mocks/Modules/ProximityMocks/StorageProximityMock.swift
@@ -14,6 +14,8 @@ class StorageProximityMock: StorageProximityInput {
     var spySaveRegion: (called: Bool, region: RegionModelOrx)!
     var spyRemoveRegion: (called: Bool, region: RegionModelOrx)!
     var spyFindElement = (called: false, code: "")
+    var spySaveRequestWaitTime = (called: false, rwt: 0)
+    var spyLoadRequestWaitTime = (called: false, rwt: 0)
     var regionInput: RegionModelOrx?
 
     func saveRegion(region: RegionModelOrx) {
@@ -28,5 +30,15 @@ class StorageProximityMock: StorageProximityInput {
         self.spyFindElement.called = true
         self.spyFindElement.code = code
         return regionInput
+    }
+    
+    func saveRequestWaitTime(rwt: Int) {
+        self.spySaveRequestWaitTime.called = true
+        self.spySaveRequestWaitTime.rwt = rwt
+    }
+    
+    func loadRequestWaitTime() -> Int? {
+        self.spyLoadRequestWaitTime.called = true
+        return nil
     }
 }
