846a31e725bf7d6bc623872ad9f606256f85836d
Eduardo Parada <eduardo.parada@gigigo.com>
2018-02-19 09:32:23 +0100

    Update: Change method of log

--- Orchextra/SDK/ORCLog/ORCLog.swift
+++ Orchextra/SDK/ORCLog/ORCLog.swift
@@ -9,45 +9,45 @@
 import Foundation
 
 protocol LogError {
-    static func logError(format: String, _ args: CVarArg...)
+    static func logError(format: String)
 }
 
 protocol LogWarning {
-    static func logWarning(format: String, _ args: CVarArg...)
+    static func logWarning(format: String)
 }
 
 protocol LogDebug {
-    static func logDebug(format: String, _ args: CVarArg...)
+    static func logDebug(format: String)
 }
 
 protocol LogVerbose {
-    static func logVerbose(format: String, _ args: CVarArg...)
+    static func logVerbose(format: String)
 }
 
 // Log Error
 extension ORCLog: LogError {
-    static func logError(format: String, _ args: CVarArg...) {
+    static func logError(format: String) {
         ORCLog.sharedInstance().logError(format)
     }
 }
 
 // Log Warning
 extension ORCLog: LogWarning {
-    static func logWarning(format: String, _ args: CVarArg...) {
+    static func logWarning(format: String) {
         ORCLog.sharedInstance().logWarning(format)
     }
 }
 
 // Log Debug
 extension ORCLog: LogDebug {
-    static func logDebug(format: String, _ args: CVarArg...) {
+    static func logDebug(format: String) {
         ORCLog.sharedInstance().logDebug(format)
     }
 }
 
 // Log Verbose
 extension ORCLog: LogVerbose {
-    static func logVerbose(format: String, _ args: CVarArg...) {
+    static func logVerbose(format: String) {
         ORCLog.sharedInstance().logVerbose(format)
     }
 }
