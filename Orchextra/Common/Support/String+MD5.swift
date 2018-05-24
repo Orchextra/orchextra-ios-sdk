86ce86674a48434450ec154ae2846db5e8bed987
José Estela <jose.estela@gigigo.com>
2018-05-24 11:30:51 +0200

    Update: Remove CommonCrypto & Add new CryptoSwift library

--- Orchextra/Common/Support/String+MD5.swift
+++ Orchextra/Common/Support/String+MD5.swift
@@ -7,19 +7,11 @@
 //
 
 import Foundation
-import CommonCrypto
+import CryptoSwift
 
 extension String {
 
     var md5: String? {
-        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
-
-        let hash = data.withUnsafeBytes { (bytes: UnsafePointer<Data>) -> [UInt8] in
-            var hash: [UInt8] = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes, CC_LONG(data.count), &hash)
-            return hash
-        }
-
-        return hash.map { String(format: "%02x", $0) }.joined()
+        return self.md5()
     }
 }
